ba30ff88aae84bc58f117f4fcbb39efb997e77f0
tjddbs0401 <48537546+tjddbs0401@users.noreply.github.com>
2022-01-19 20:58:56 +0900

    message

--- blog-config.js
+++ blog-config.js
@@ -1,14 +1,14 @@
 module.exports = {
-  title: "hoodie",
-  description: "Hello :) I'm Hudi who developed gatsby-starter-hoodie theme.",
-  author: "Hudi",
+  title: "Singsangsungyoon",
+  description: "Hello :) I'm Sungyoon Cho who is the writer of this blog.",
+  author: "Sungyoon",
   siteUrl: "https://devhudi.github.io/gatsby-starter-hoodie/",
   links: {
-    github: "https://github.com/devHudi",
+    github: "https://github.com/tjddbs0401",
     linkedIn: "https://linkedin.com",
     facebook: "https://www.facebook.com",
-    instagram: "https://www.instagram.com",
-    email: "mailto:devhudi@gmail.com",
+    instagram: "https://www.instagram.com/singsangsungyoon/",
+    email: "mailto:tjddbs040112@gmail.com",
   },
   utterances: {
     repo: "devHudi/gatsby-starter-hoodie",
