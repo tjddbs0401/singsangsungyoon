4e1296c429f72aaa71f84b3e649af3274751050c
tjddbs0401 <tjddbs040112@gmail.com>
2022-07-02 23:23:10 +0900

    profile update

--- blog-config.js
+++ blog-config.js
@@ -1,11 +1,11 @@
 module.exports = {
   title: "Singsangsungyoon",
-  description: "Hello :) I'm Sungyoon Cho who is the writer of this blog.",
+  description: "Upcoming freshman in NYU(year 2026) pursuing the joint major of CS and DS (b.a.). \n Interested in machine learning, recommendation & personalization, media(photography), and problem-solving of inequality in education via technology.",
   author: "Sungyoon",
   siteUrl: "https://devhudi.github.io/gatsby-starter-hoodie/",
   links: {
     github: "https://github.com/tjddbs0401",
-    linkedIn: "https://linkedin.com",
+    linkedIn: "https://www.linkedin.com/in/singsangsungyoon/",
     facebook: "https://www.facebook.com",
     instagram: "https://www.instagram.com/singsangsungyoon/",
     email: "mailto:tjddbs040112@gmail.com",
